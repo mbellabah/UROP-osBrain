8f30e0b22bd9dcc9a9ecad6f4f8ee5af0ecb5b42
BellaBah <bellabah@mit.edu>
2019-06-27 15:36:20 -0500

    Implemented parallel PAC?

--- Main.py
+++ Main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     main_class = Main(n_nodes=3)
     main_class.setup_atoms()
 
-    # main_class.run_PAC(T=10)
+    main_class.run_PAC()
--- Worker.py
+++ Worker.py
@@ -25,28 +25,23 @@ class Node(Agent):
         self.atom = None
         self.neighbors = list(set([1, 2, 3]) - {self.atom_id})     # list(self.atom._neighbors.keys()) FIXME
 
-    def initial_broadcast_y(self):
-        # assumes self.atom exists
-        self.log_info(f'(1) {self.global_atom_y}')
+        self.round = 0
+        self.neighbor_rounds = {}
 
-        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': 'y', 'payload': self.atom.get_y()}
+    # MARK: Initializing
+    def initial_broadcast_y(self):
+        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': 'y', 'payload': self.atom.get_y(), 'round': self.round}
         for j in self.neighbors:
             self.broadcast(msg=data_package, recipient=f'Node-{j}')
             node_reply = self.recv(f'Node-{j}')
-
-            msg_type = node_reply['msg_type']
-            sender = node_reply['sender']
-            payload = node_reply['payload']
-
-            self.set_values(msg_type, sender, payload)
-
-        self.log_info(f'(2) {self.global_atom_y}')
+            self.set_values(**node_reply)
 
     def initial_broadcast_nu_bar(self):
-        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': 'nu_bar', 'payload': self.atom.get_nu_bar()}
+        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': 'nu_bar', 'payload': self.atom.get_nu_bar(), 'round': self.round}
         for j in self.neighbors:
             self.broadcast(msg=data_package, recipient=f'Node-{j}')
-            _ = self.recv(f'Node-{j}')
+            node_reply = self.recv(f'Node-{j}')
+            self.set_values(**node_reply)
 
     def init_dual_vars(self):
         self.atom.init_dual_vars()
@@ -60,41 +55,46 @@ class Node(Agent):
 
     def reply_to_node(self, data_package: dict):
 
-        # Receive
-        sender: int = data_package['sender']
-        msg_type = data_package['msg_type']
-        payload: np.array = data_package['payload']
-
-        self.set_values(msg_type, sender, payload)
-
-        # Test 1
-        # self.log_info(f'{self.name}, {data_package}')
+        # Receive and set
+        self.set_values(**data_package)
 
         # Reply
-        data_package = self.compose_package(msg_type=msg_type)
+        data_package = self.compose_package(msg_type=data_package['msg_type'])
         return data_package
 
     def broadcast_all(self, msg: Dict[str, Any]) -> None:
         for node_id in self.neighbors:
             self.broadcast(msg=msg, recipient=f'Node-{node_id}')
             node_reply = self.recv(f'Node-{node_id}')
-            self.log_info(f'{self.name}: {node_reply}')
+            # self.log_info(f'node: {node_reply["sender"]} is on round: {node_reply["round"]}')
+            self.set_values(**node_reply)
+
+        # Synchrony achieved
+        if msg['msg_type'] == 'y':
+            self.atom.update_y_and_mu()
+        elif msg['msg_type'] == 'nu_bar':
+            self.atom.update_nu()
+
+        self.round += 1
 
     def receive_setup(self, data_package: dict):
         if 'is_setup' in data_package:      # receives from coordinator; {'is_setup': bool, 'data': dict}
             self.log_info('received setup info from coordinator')
             self.atom = Atom(atom_id=self.atom_id, node_data_package=data_package['data'])
 
-    def set_values(self, msg_type: str, sender: int, payload: dict):
+    # MARK: Utility
+    def set_values(self, msg_type: str, sender: str, payload: dict, round: int):
+        formatted_sender: int = int(sender[5:])
+        self.neighbor_rounds[sender] = round
         if msg_type == 'y':
             self.global_atom_y[sender] = payload
-            self.atom.global_atom_y[sender] = payload
+            self.atom.global_atom_y[formatted_sender] = payload
         elif msg_type == 'nu_bar':
             self.global_atom_nu_bar[sender] = payload
-            self.atom.global_atom_nu_bar[sender] = payload
+            self.atom.global_atom_nu_bar[formatted_sender] = payload
 
     def compose_package(self, msg_type) -> dict:
-        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': msg_type}
+        data_package = {'sender': f'Node-{self.atom_id}', 'msg_type': msg_type, 'round': self.round}
         if msg_type == 'y':
             data_package['payload'] = self.atom.get_y()
         elif msg_type == 'nu_bar':
@@ -102,8 +102,15 @@ class Node(Agent):
 
         return data_package
 
-    def run_pac(self):
-        pass
+    def is_synchronized(self):
+        for round in self.neighbor_rounds.values():
+            if self.round != round:
+                return False
+        return True
+
+    def update_pac(self):
+        # Broadcast and handle the response for each
+        self.broadcast_all(msg=self.compose_package(msg_type='y'))
 
 
 class Coordinator(Agent):
@@ -163,29 +170,20 @@ class Main:
         # Broadcast initial
         for node in self.node_dict.values():
             node.initial_broadcast_y()
-        # for node in self.node_dict.values():
-        #     node.init_dual_vars()
-        # for node in self.node_dict.values():
-        #     node.initial_broadcast_nu_bar()
-
-        self.ns.shutdown()
-
-    def run_PAC(self, T):
-
-        # # for t in range(1, T+1):
-        # #     print(f'Round: {t}/{T}')
-        # for node in self.node_dict.values():
-        #     node.periodic()
-        #
-        # time.sleep(T)
-        #
-        # for node in self.node_dict.values():
-        #     print(node.get_attr('atom')._y)
-
         for node in self.node_dict.values():
-            node.run_pac()
+            node.init_dual_vars()
+        for node in self.node_dict.values():
+            node.initial_broadcast_nu_bar()
 
-        time.sleep(10)
+    def run_PAC(self, T=100):
+
+        for t in range(T):
+            print(f'Round {t}/{T}')
+            for node in self.node_dict.values():
+                node.update_pac()
+
+        for node_name, node in self.node_dict.items():
+            print(node_name, node.get_attr('atom')._y)
 
         # Terminate
         self.ns.shutdown()
